6ef77c77ff7e8e1d704c138b33f68d4673e1eb0f
Khushnood Ali Khan <khushnoodali287@gmail.com>
2024-05-09 14:52:02 +0500

    Function Overloading

---
+++ function_overloading.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int funct(int a, int b){
+    return (a*b);
+}
+float funct(float a, float b){
+    return (a/b);
+}
+main(){
+    int num1, num2;
+    float num3, num4;
+    cout<<"Enter Num 1: ";
+    cin>>num1;
+    cout<<"Enter Num 2: ";
+    cin>>num2;
+    cout<<funct(num1,num2);
+    cout<<"\nEnter Num 3: ";
+    cin>>num3;
+    cout<<"Enter Num 4: ";
+    cin>>num4;
+    cout<<funct(num3,num4);
+}
